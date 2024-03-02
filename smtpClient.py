61f888fd1ba05b28c48b1d45f91477874f734c55
Sharoon Reyes <158117960+skr2401@users.noreply.github.com>
2024-03-01 20:48:36 -0500

    Add files via upload

--- smtpClient.py
+++ smtpClient.py
@@ -8,31 +8,35 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
 
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
-
+    mailServer = ('127.0.0.1', 1025)
     # Fill in start
     clientSocket = socket(AF_INET, SOCK_STREAM)
     #smtp = gmail.com(127.0.0.1) and port=1025
-        clientSocket.connect ((127.0.0.1, 1025))
+    clientSocket.connect(mailServer)
+    # mailserver = ('127.0.0.1', 1025)
+    #clientSocket.connect((mailserver))
     # Fill in end
 
-    recv = clientSocket.recv(1024).decode()
+    recv = clientSocket.recv(1024)
+    recv = recv.decode()
     #print
-    print(recv) #You can use these print statement to validate return codes from the server.
+    print("Message after connection request:" + recv) #You can use these print statement to validate return codes from the server.
     if recv[:3] != '220':
         print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
-    recv1 = clientSocket.recv(1024).decode()
+    recv1 = clientSocket.recv(1024)
+    recv1 = recv1.decode()
     #print
-    print(recv1) 
+    print("Message after Helo command:" + recv1) 
     if recv1[:3] != '250':
         print('250 reply not received from server.')
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
-    mailFrom = "Mail From: <sender@gmail.com>\r\n"
+    mailFrom = "Mail From:<sender@gmail.com>\r\n"
     clientSocket.send(mailFrom.encode())
     recv2 = clientSocket.recv(1024).decode()
     #print
@@ -59,24 +63,27 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send message data.
     # Fill in start
+
+    date = date + "\r\n\r\n"
+    clientSocket.send(date.encode())
     clientSocket.send(msg.encode())
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
     clientSocket.send(endmsg.encode())
-    recv5 = cientSocket.recv(1024).decode()
+    recv5 = clientSocket.recv(1024)
     #print
-    print("Response after sending message body: " + recv5)
+    print("Response after sending message body: " + recv5.decode())
     # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
     quitCommand = "QUIT\r\n"
     clientSocket.send(quitCommand.encode())
-    recv6 = clientSocket.recv(1024).decode()
+    recv6 = clientSocket.recv(1024)
     #print
-    print(recv6)
+    print(recv6.decode())
     # Fill in end
 
     # Close the socket ********
